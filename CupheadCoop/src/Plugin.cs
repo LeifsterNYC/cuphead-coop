6fe3c73a023fffbecdceab5f4f28b4848dc415f7
Leif Rogers <leifrrogers@gmail.com>
2026-05-10 12:11:31 -0400

    v0.9.1: remote-player motor bypass on client (Germanized-style PlayerMotorPatch adoption)

--- CupheadCoop/src/Plugin.cs
+++ CupheadCoop/src/Plugin.cs
@@ -18,7 +18,7 @@ namespace CupheadCoop
     public class Plugin : BaseUnityPlugin
     {
         public const string GUID = "leif.cupheadcoop";
-        public const string Version = "0.9.0";
+        public const string Version = "0.9.1";
 
         private Harmony _harmony;
         private CoopHost _host;
